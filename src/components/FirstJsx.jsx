3481f1117eccc816d72005b56df98ab8705896a4
devbrown23 <devbrownrealtor@gmail.com>
2022-07-07 10:00:09 -0700

    1st comment

---
+++ src/components/FirstJsx.jsx
@@ -0,0 +1,25 @@
+///import Dependency//
+import React, { Component } from "react";
+
+//Create the component//
+
+class FirstJsx extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo!</h1>
+                <h2>Things I need to do :</h2>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathonn</li>
+                <li>Feed the dogs</li>
+            </div>
+            
+        );
+    }
+
+}
+
+//export the component//
+
+export default FirstJsx;
